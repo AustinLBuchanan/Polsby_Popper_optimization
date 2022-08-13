8576d6ec48a1a19d82309d169c319bfb74417539
Pietro Belotti <petr7b6@gmail.com>
2022-08-13 17:18:37 +0200

    redoing change on is_cut variables added to the general problem

--- src/mip_objective.py
+++ src/mip_objective.py
@@ -5,12 +5,6 @@ import math
 
 def add_cut_edges_objective(m, DG):
 
-    # add strengthening vars/constraints:
-    # edge {u,v} is cut <=> arc (u,v) is cut <=> arc (v,u) is cut
-    undirected_edges = [ (u,v) for u,v in DG.edges if u<v ]
-    m._is_cut = m.addVars( undirected_edges, name='iscut', vtype=GRB.BINARY )
-    m.addConstrs( m._is_cut[min(u,v),max(u,v)] == gp.quicksum( m._y[u,v,j] for j in range(DG._k) ) for u,v in DG.edges )
-
     m.setObjective( gp.quicksum( m._is_cut ), GRB.MINIMIZE )
     return
 
