d755cb6779f4c6e15bd7d0f12b29cb64545f956a
Austin Buchanan <36489966+AustinLBuchanan@users.noreply.github.com>
2024-05-22 11:17:47 -0500

    Add files via upload

--- src/read.py
+++ src/read.py
@@ -10,13 +10,13 @@ def read_graph_from_json(json_file):
     for i in G.nodes:
         G.nodes[i]['TOTPOP'] = G.nodes[i]['P0010001'] 
     
-    # change meters to kilometers for better numerical performance in gurobi
-    th = 1000
+    # change distance units from meters to 100km for better numerics in gurobi
+    ht = 100000  
     for i in G.nodes:
-        G.nodes[i]['area'] /= ( th * th )
+        G.nodes[i]['area'] /= ( ht * ht )
         if G.nodes[i]['boundary_node']:
-            G.nodes[i]['boundary_perim'] /= th
+            G.nodes[i]['boundary_perim'] /= ht
     for i,j in G.edges:
-        G.edges[i,j]['shared_perim'] /= 1000
+        G.edges[i,j]['shared_perim'] /= ht
         
     return G
