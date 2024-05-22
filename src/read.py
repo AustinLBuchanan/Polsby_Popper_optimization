447b8065b5f85c7c02bf3e60d4a9e51e8d46e326
Austin Buchanan <36489966+AustinLBuchanan@users.noreply.github.com>
2024-05-22 07:28:25 -0500

    Add files via upload

--- src/read.py
+++ src/read.py
@@ -4,4 +4,19 @@ from networkx.readwrite import json_graph
 def read_graph_from_json(json_file):
     with open(json_file) as f:
         data = json.load(f)
-    return json_graph.adjacency_graph(data) 
+    G = json_graph.adjacency_graph(data) 
+
+    # total population
+    for i in G.nodes:
+        G.nodes[i]['TOTPOP'] = G.nodes[i]['P0010001'] 
+    
+    # change meters to kilometers for better numerical performance in gurobi
+    th = 1000
+    for i in G.nodes:
+        G.nodes[i]['area'] /= ( th * th )
+        if G.nodes[i]['boundary_node']:
+            G.nodes[i]['boundary_perim'] /= th
+    for i,j in G.edges:
+        G.edges[i,j]['shared_perim'] /= 1000
+        
+    return G
