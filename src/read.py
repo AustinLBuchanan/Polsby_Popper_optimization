6765a698a19ea3e2262f04987ef0f56192c53cfb
Austin Buchanan <36489966+AustinLBuchanan@users.noreply.github.com>
2024-05-23 07:42:16 -0500

    Add files via upload

--- src/read.py
+++ src/read.py
@@ -1,22 +1,25 @@
 import json
 from networkx.readwrite import json_graph
 
-def read_graph_from_json(json_file):
+def read_graph_from_json(json_file, update_population=True, rescale_distance=True):
     with open(json_file) as f:
         data = json.load(f)
     G = json_graph.adjacency_graph(data) 
 
-    # total population
-    for i in G.nodes:
-        G.nodes[i]['TOTPOP'] = G.nodes[i]['P0010001'] 
+    if update_population:
+        # total population
+        for i in G.nodes:
+            G.nodes[i]['TOTPOP'] = G.nodes[i]['P0010001'] 
     
     # change distance units from meters to 100km for better numerics in gurobi
-    ht = 100000  
-    for i in G.nodes:
-        G.nodes[i]['area'] /= ( ht * ht )
-        if G.nodes[i]['boundary_node']:
-            G.nodes[i]['boundary_perim'] /= ht
-    for i,j in G.edges:
-        G.edges[i,j]['shared_perim'] /= ht
+    #  note: 100km roughly corresponds to one degree of latitude
+    if rescale_distance:
+        ht = 100000  
+        for i in G.nodes:
+            G.nodes[i]['area'] /= ( ht * ht )
+            if G.nodes[i]['boundary_node']:
+                G.nodes[i]['boundary_perim'] /= ht
+        for i,j in G.edges:
+            G.edges[i,j]['shared_perim'] /= ht
         
     return G
